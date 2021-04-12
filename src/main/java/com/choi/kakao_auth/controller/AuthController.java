cec00018df8e112ad40e75a52be7af6949e2476f
choi0926 <ghtjd753753@gmail.com>
2021-04-12 15:41:56 +0900

    naver auth 추가

--- src/main/java/com/choi/kakao_auth/controller/AuthController.java
+++ src/main/java/com/choi/kakao_auth/controller/AuthController.java
@@ -3,8 +3,10 @@ package com.choi.kakao_auth.controller;
 
 import com.choi.kakao_auth.service.GoogleService;
 import com.choi.kakao_auth.service.KakaoService;
+import com.choi.kakao_auth.service.NaverService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import lombok.var;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,8 +21,10 @@ public class AuthController {
 
     private final GoogleService googleService;
 
-    @RequestMapping(value = "/login/auth")
-    public String home(@RequestParam(value = "code", required = false) String code) throws Exception{
+    private final NaverService naverService;
+
+    @RequestMapping(value = "/auth/kakao")
+    public String kakao(@RequestParam(value = "code", required = false) String code) throws Exception{
         String access_Token = kakaoService.getAccessToken(code);
         HashMap<String, Object> userInfo = kakaoService.getUserInfo(access_Token);
         System.out.println("###access_Token#### : " + access_Token);
@@ -33,7 +37,20 @@ public class AuthController {
     public String google(@RequestParam(value = "code", required = false) String code) throws Exception{
         String access_Token = googleService.getAccessToken(code);
         HashMap<String, Object> userInfo = googleService.getUserInfo(access_Token);
-        log.debug("dd");
         return "google";
+    }
+
+    @RequestMapping(value = "/login/auth/naver")
+    public String naver(){
+        return "naver";
+    }
+
+
+    @RequestMapping(value = "/auth/naver")
+    public String naver_post(@RequestParam(value = "code", required = false) String code,
+                        @RequestParam(value = "state", required = false) String state) throws Exception{
+        String access_Token = naverService.getAccessToken(code,state);
+        naverService.getUserInfo(access_Token);
+        return "/auth_naver";
     }
 }
---
+++ src/main/java/com/choi/kakao_auth/service/NaverService.java
@@ -0,0 +1,152 @@
+package com.choi.kakao_auth.service;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class NaverService {
+
+
+    @Value("${naver.client-id}")
+    private String clientId;
+
+    @Value("${naver.client-secret}")
+    private String clientSecret;
+
+    @Value("${naver.redirect_uri}")
+    private String redirectUri;
+
+    public String getAccessToken (String authorize_code, String state) {
+        String access_Token = "";
+        String refresh_Token = "";
+        String expires_in = "";
+        String reqURL = "https://nid.naver.com/oauth2.0/token";
+
+        try {
+            URL url = new URL(reqURL);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            //    POST 요청을 위해 기본값이 false인 setDoOutput을 true로
+
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+
+            //    POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            StringBuilder sb = new StringBuilder();
+            sb.append("grant_type=authorization_code");
+            sb.append("&client_id="+clientId);  //본인이 발급받은 key
+            sb.append("&client_secret="+clientSecret);
+            sb.append("&redirect_uri="+redirectUri);     // 본인이 설정해 놓은 경로
+            sb.append("&code=" + authorize_code);
+            sb.append("&state=" + state);
+            bw.write(sb.toString());
+            bw.flush();
+
+            System.out.println(sb);
+            //    결과 코드가 200이라면 성공
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode : " + responseCode);
+
+            //    요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("response body : " + result);
+
+            //    Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+            access_Token = element.getAsJsonObject().get("access_token").getAsString();
+
+            br.close();
+            bw.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+          return access_Token;
+    }
+
+    public void getUserInfo(String token){
+        System.out.println(token);
+        String header = "Bearer " + token; // Bearer 다음에 공백 추가
+
+        String apiURL = "https://openapi.naver.com/v1/nid/me";
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("Authorization", header);
+        String responseBody = get(apiURL,requestHeaders);
+        System.out.println("responseBody: "+responseBody);
+    }
+
+    private static String get(String apiUrl, Map<String, String> requestHeaders){
+        HttpURLConnection con = connect(apiUrl);
+        try {
+            con.setRequestMethod("GET");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+                return readBody(con.getInputStream());
+            } else { // 에러 발생
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+
+
+    private static HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+
+    private static String readBody(InputStream body){
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+        }
+    }
+}
